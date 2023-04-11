501c857279c4f1006eb8620604b46334076b1820
PiyumalNipuna60 <nipuna315np@gmail.com>
2023-04-12 05:01:00 +0530

    add  content

--- fb-clone/src/component/story/Story.js
+++ fb-clone/src/component/story/Story.js
@@ -1,11 +1,11 @@
 import React from 'react'
-import Avatar from '@mui/material'
+import { Avatar } from '@mui/material'
 
 
 function Story({image,profileSrc,title}) {
   return (
     <div 
-     style={{backgroundImage:`url($(image))`,
+     style={{backgroundImage:`url(${image})`,
      backgroundRepeat:"no-repeat"
     }}
     className='story'>
